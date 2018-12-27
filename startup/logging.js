2716b5373d89fefd32e84a5a0db5193d855a544a
Vlady <vladya912d@gmail.com>
2018-12-27 20:43:57 +0200

    config

--- startup/logging.js
+++ startup/logging.js
@@ -2,7 +2,15 @@ const winston = require('winston');
 require('express-async-errors');
 
 module.exports = function() {
-  winston.handleExceptions(
+  const logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.json(),
+    transports: [
+      new winston.transports.Console(),
+      new winston.transports.File({ filename: 'logfile.log' })
+    ]
+  });
+  winston.exceptions.handle(
     new winston.transports.Console({ colorize: true, prettyPrint: true }),
     new winston.transports.File({ filename: 'uncaughtExceptions.log' }));
   
